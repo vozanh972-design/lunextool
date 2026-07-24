2f48dcc465fcef9569c41c921db39cc2a7e1df80
vozanh972-design <vozanh972@gmail.com>
2026-07-24 12:43:47 +0700

    Add files via upload

--- app/src/main/java/com/cayxu/app/ui/screens/golike/GolikeLoginScreen.kt
+++ app/src/main/java/com/cayxu/app/ui/screens/golike/GolikeLoginScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
