efe8dcfc846d7f4bc3c3d5cde98905cee5da0118
thenah <vozanh972@gmail.com>
2026-09-25 18:26:24 +0700

    Import horizontalScroll in TuongTacCheoScreen

--- app/src/main/java/com/cayxu/app/ui/screens/tasks/TuongTacCheoScreen.kt
+++ app/src/main/java/com/cayxu/app/ui/screens/tasks/TuongTacCheoScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
